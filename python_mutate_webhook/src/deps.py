3bbaa3320980433c8090df8b9215e102eb4e57a4
zhoupoko2000 <maria@wajam.com>
2021-11-19 20:46:30 -0500

    Delete python_mutate_webhook directory

--- python_mutate_webhook/src/deps.py
+++
@@ -1,9 +0,0 @@
-def setup_deps():
-    import sys
-    from pathlib import Path
-    BASE_DIR = Path(__file__).resolve().parents[0]
-    LIB_DIR = str(BASE_DIR.joinpath("lib"))
-    sys.path.insert(0, str(BASE_DIR))
-    sys.path.insert(0, str(LIB_DIR))
-
-setup_deps()
